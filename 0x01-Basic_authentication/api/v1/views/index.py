bec0371db7b25097a189695d213d34b69908f974
Oussama Chaouki <oussama11chaouki@gmail.com>
2024-07-08 17:04:30 +0000

    1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized():
     This endpoint must raise a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    This endpoint must raise a 403 error by using abort
+    """
+    abort(403)
