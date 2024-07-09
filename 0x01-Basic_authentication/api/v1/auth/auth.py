709446688bf66c26a37f7d682078504b0c975383
Oussama Chaouki <oussama11chaouki@gmail.com>
2024-07-09 23:16:14 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,9 +2,6 @@ from flask import request
 from typing import List, TypeVar
 
 
-User = TypeVar('User')
-
-
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         # path and excluded_paths will be used later
@@ -15,6 +12,6 @@ class Auth:
         # request will be the Flask request object
         return None
 
-    def current_user(self, request=None) -> User:
+    def current_user(self, request=None) -> TypeVar('User'):
         # request will be the Flask request object
         return None
